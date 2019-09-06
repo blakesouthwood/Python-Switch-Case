5f6b5ceae7cdfc521893aad79f34a46663c7a76a
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2019-09-05 17:27:53 -0700

    Add files via upload
    I was deeply saddened to learn that Python didn't have switch case so I put one together that is
    pleasant on the eyes and it works and it's simple. Einstein said simplicity is beauty. 
    If you add a function call inside each of the cases you can have pages of code run in each case if you wish.

---
+++ switch_case.py
@@ -0,0 +1,34 @@
+# Blake Southwood Software Engineer
+# Sept 5, 2019 Silicon Valley
+#C and C++ and JavaScript have switch case
+# and now Python has it(sort of) easier on the eyes
+# then bland if elif else
+# it looks nice especially in Atom and Sublime
+
+
+#value you want to run thru switch case
+x = "phrase"
+
+case = switch(x)
+while True:
+    if  case == "one":
+        print("yes it's one")
+        break;
+    elif case == "wilson":
+        print ("we have a wilson winner")
+        break;
+    elif case == "rudolph":
+        print ("go reindeer")
+        break;
+    elif case == "phrase":
+        print ("testing")
+        break;
+    else: #default
+        print('none ')
+        break;
+#end switch
+
+    
+
+ def switch(x):
+    return x;
