5d2a47f617789090421fb63e09ecb65bd917e455
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2019-09-22 10:47:14 -0700

    Add files via upload

--- switch_case.py
+++ switch_case.py
@@ -10,7 +10,7 @@
 # and change the case value after the triggered case value but
 # before and above using a continue do this: case = "newvalue"
 # just added fallthru() Sept 9th, 2019. if you use fallthru() don't use break in that case
-# if you forget to use a  "break" you will get an infinite loop
+
 # unless you use a fallthru which doesn't need a break after it
 # at this point you can only use one fallthru inside of a case
 # remember that when using fallthru you can't have a break after it
@@ -63,7 +63,7 @@ def testfunction(x):
 # ========  switch case code =======================
     switch(x)                    #<<====== switch() method is here
 
-    while True:                  #<<====== infinite loop to use breaks
+    for i in range(1):                  #<==== loop once to use breaks
         if   case  == "one":
             print("yes it's one")
             break
@@ -91,7 +91,7 @@ def testfunction(x):
     #default:
         else:
             print('none')
-            break         #<<==== this break is necessary to leave the loop
+            break         
 #end loop
 #end switch
 
