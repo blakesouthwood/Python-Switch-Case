ec234f13c29001c8faaa904d9cd57beb1399c5ee
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2022-10-08 14:30:43 -0700

    Add files via upload

--- book_writing.py
+++ book_writing.py
@@ -1,3 +1,74 @@
+
+I never thought any of this was possible. My whole 
+view of reality is forever changed. Nothing seems real now.
+I hunger for music to tune out reality and create my own
+reality so I don't have time to think about what is going on.
+
+But first how it began. I visited the Hollister Horse Ranch
+in the autumn of 2017 after a facebook post by my friend
+Marilyn's sister Terry who said why not come visit and
+I did - later I was offered a job of caring for the horses
+for room and board which ended up being for 2 years.  What
+she neglected to mention was back breaking yard work and 
+landscaping and shoveling manure. 
+
+I was working on a horse ranch in Hollister taking care of
+a dozen horses feeding, watering, and graining them and
+felt like I was being held in a Death Camp in Nazi Germany
+with Nurse Ratchet/Hilter for a landlord. I put up with
+all of this nonsence to get free rent and use of a F350 pickup
+that got 7 miles per gallon that I could use to work at
+Walmart in Gilroy.
+
+In my mind Hollister was bigger than the state of Texas. It was
+a seven mile hike to Starbucks which I took on my days off.
+I only discovered the County Express Shuttle after several months
+of the long walk that would take me two hours. The reason that
+I was going to Starbucks was so that I could code and use fast
+wi-fi. I wanted to get away from the horses and flies and the
+general nothingness of the five acre ranch that felt like a
+prision camp to me. I was soon watching movies that refelected
+what I was enduring: The Great Escape, One Flew Over the Cuckoo's
+Nest, and  The Shawshank Redemption. I hungered for freedom. 
+
+I was so desperate to leave that I studied hard and took the
+Florida Teacher Certification Exam and passed it on the first
+attempt and even flew to Tampa, Florida to be interviewed but
+it was pretty scary there and so I resolved I better stay
+in California where my friends were. What kept me going was
+working on my Python coding. My dream was to get a programming
+job so that I could leave the hell ranch in Hollister.
+
+When I arrived  in April 2018 at the Horse Ranche after an overnight
+train ride from Salem, Oregon where I spent the past year writing
+JavaScript and applying for jobs without success. I intially 
+immediately got a job as a stocker at Target in Hollister but it
+only offered at most 16 hours per week which was unstastainable.
+
+I had noticed the article in the New York Times about the Navy 
+Tic Tac encounter near the USS Enteprise south of San Diego 
+in 2004.I thought wow cool ufos without zero thought whatsoever
+about the aliens. And I started watching the New York Post
+Youtube channel called the Basement Files and each time it seemed
+like there was scant evidence. 
+
+So other than watching the movie Paul which I first saw in Oregon
+and I walked out of the living room because I was convinced it
+was just too far fetched I really had no interest in the matter.
+I had enjoyed the Star Wars movies in terms of science fiction
+but I was not an active reader of sci-fi novels.
+
+
+
+
+
+
+
+
+
+
+
+
 book writing beginning on October 8th, 2022 9:28am
 
 testing adding more stuff
