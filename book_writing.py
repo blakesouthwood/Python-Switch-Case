c4a355cb2f95eccf764c3092b443f2e94e605454
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2022-10-08 09:54:53 -0700

    Add files via upload

--- book_writing.py
+++ book_writing.py
@@ -7,3 +7,7 @@ let's see how it goes nice quiet serene environment
 adding this line at the bottom for testing purposes
 
 in your eyes test
+
+yet another test to make sure that this is working
+corectly and I have no issues with it. No annoying ads.
+
