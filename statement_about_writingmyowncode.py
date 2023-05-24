6ad36e9befdd9c8b935c591398caf01f31b4489d
blakesouthwood <47462525+blakesouthwood@users.noreply.github.com>
2023-05-23 23:37:46 -0700

    I felt the need to claim ownership for my code
    Since it is technically a parser and codegen and
    it has details on implementing macros that work
    in python and nested switch cases and duplicating
    the switch case behavior in JavaScript and C
    I wanted the world to know I thought it up and
    implemented it.

---
+++ statement_about_writingmyowncode.py
@@ -0,0 +1,20 @@
+about copilot.
+
+I didn't use copilot to write any of my code.
+I haven't used copilot yet.
+I did get help making a few small behaviors from Stack Overflow
+that I mention in my code.
+
+So I wanted to make it clear that 99.9% of the code
+that I wrote I did actually write myself.
+
+Also I am starting a new project conquering the No Silver Bullet
+proposed by Fred Brooks based on Dijkstra's Computer Assited
+Programming paper and Dijkstra's Turing Lecture paper and
+Don Knuth's Literate Programming paper.
+
+Working on a quick and dirty prototype now in my spare time
+when I'm not studying for my tests to get into teaching.
+
+Blake Southwood
+
